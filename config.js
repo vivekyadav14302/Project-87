6e2b011a6a73f86a8f7c829089c48cf221c5b140
Vivek Yadav <88832797+vivekyadav14302@users.noreply.github.com>
2022-05-15 12:02:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOKz721pzviTl4w3vJtZpWZPnFpJ6u5ME",
+  authDomain: "storytelling-app-f03eb.firebaseapp.com",
+  databaseURL: "https://storytelling-app-f03eb-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-f03eb",
+  storageBucket: "storytelling-app-f03eb.appspot.com",
+  messagingSenderId: "281553066875",
+  appId: "1:281553066875:web:297b641865a6735005bbde",
+  measurementId: "G-Y541FLS42K"
+};
